c5e984fe1f00218fc79eeca0c4af20e656a2460b
Harry Zhang <harryz@hyper.sh>
2016-08-13 08:50:17 -0400

    Reomve pkg/client/typed/generated & winio

--- pkg/storage/testing/codecgen-pkg-1234.generated.go
+++ pkg/storage/testing/codecgen-pkg-1234.generated.go
@@ -1,13 +1,28 @@
+/*
+Copyright 2015 The Kubernetes Authors All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 
 package testing
 
 import (
+	"bytes"
 	codec1978 "github.com/ugorji/go/codec"
+	"go/format"
 	"os"
 	"reflect"
-	"bytes"
 	"strings"
-	"go/format"
 )
 
 func CodecGenTempWrite1234() {
@@ -17,8 +32,8 @@ func CodecGenTempWrite1234() {
 	}
 	defer fout.Close()
 	var out bytes.Buffer
-	
-	var typs []reflect.Type 
+
+	var typs []reflect.Type
 
 	var t0 TestResource
 	typs = append(typs, reflect.TypeOf(t0))
@@ -31,4 +46,3 @@ func CodecGenTempWrite1234() {
 	}
 	fout.Write(bout)
 }
-
