368479f578b7f7986c62634fc76471838bc4d260
Sanjeev Mehrotra <sanjeevm0@hotmail.com>
2017-04-04 13:58:57 -0700

    Add more unit tests for allocator

--- plugin/pkg/scheduler/algorithm/predicates/grpallocate_test.go
+++ plugin/pkg/scheduler/algorithm/predicates/grpallocate_test.go
@@ -3,13 +3,30 @@ package predicates
 import (
 	"flag"
 	"fmt"
+	"math"
 	"testing"
 
 	"github.com/golang/glog"
 
+	"regexp"
+
 	v1 "k8s.io/kubernetes/pkg/api"
 	"k8s.io/kubernetes/plugin/pkg/scheduler/schedulercache"
 )
+
+type cont struct {
+	name           string
+	res            map[string]int64
+	grpres         map[string]int64
+	expectedGrpLoc map[string]string
+}
+
+type PodEx struct {
+	pod           *v1.Pod
+	expectedScore float64
+	icont         []cont
+	rcont         []cont
+}
 
 func printContainerAllocation(cont *v1.Container) {
 	//glog.V(5).Infoln("Allocated", cont.Resources.Allocated)
@@ -70,7 +87,7 @@ func createNode(name string, res map[string]int64, grpres map[string]int64) *sch
 	alloc := v1.ResourceList{}
 	setResource(alloc, res, grpres)
 	node := v1.Node{ObjectMeta: v1.ObjectMeta{Name: name}, Status: v1.NodeStatus{Capacity: alloc, Allocatable: alloc}}
-	fmt.Println("AA", node.Status.Allocatable)
+	//fmt.Println("AA", node.Status.Allocatable)
 	nodeInfo := schedulercache.NewNodeInfo()
 	nodeInfo.SetNode(&node)
 
@@ -79,33 +96,47 @@ func createNode(name string, res map[string]int64, grpres map[string]int64) *sch
 	return nodeInfo
 }
 
-type cont struct {
-	name   string
-	res    map[string]int64
-	grpres map[string]int64
+func setExpectedResources(c *cont) {
+	expectedGrpLoc := make(map[string]string)
+	for key, val := range c.expectedGrpLoc {
+		re := regexp.MustCompile(key + `/(.*)`)
+		for keyRes := range c.grpres {
+			matches := re.FindStringSubmatch(keyRes)
+			if len(matches) >= 2 {
+				newKey := v1.ResourceGroupPrefix + "/" + key + "/" + matches[1]
+				newVal := v1.ResourceGroupPrefix + "/" + val + "/" + matches[1]
+				expectedGrpLoc[newKey] = newVal
+			}
+		}
+	}
+	c.expectedGrpLoc = expectedGrpLoc
 }
 
-func createPod(name string, iconts []cont, rconts []cont) *v1.Pod {
+func createPod(name string, expScore float64, iconts []cont, rconts []cont) (*v1.Pod, *PodEx) {
 	pod := v1.Pod{ObjectMeta: v1.ObjectMeta{Name: name}, Spec: v1.PodSpec{}}
 	spec := &pod.Spec
 
 	var contR v1.ResourceList
 
 	for index, icont := range iconts {
+		setExpectedResources(&iconts[index])
 		contR = addContainer(&spec.InitContainers, icont.name)
 		setResource(contR, icont.res, icont.grpres)
 		glog.V(7).Infoln(icont.name, spec.InitContainers[index].Resources.Requests)
 	}
 	for index, rcont := range rconts {
+		setExpectedResources(&rconts[index])
 		contR = addContainer(&spec.Containers, rcont.name)
 		setResource(contR, rcont.res, rcont.grpres)
 		glog.V(7).Infoln(rcont.name, spec.Containers[index].Resources.Requests)
 	}
 
-	return &pod
+	podEx := PodEx{pod: &pod, icont: iconts, rcont: rconts, expectedScore: expScore}
+
+	return &pod, &podEx
 }
 
-func sampleTest(pod *v1.Pod, nodeInfo *schedulercache.NodeInfo) {
+func sampleTest(pod *v1.Pod, podEx *PodEx, nodeInfo *schedulercache.NodeInfo) {
 	// now perform allocation
 	spec := &pod.Spec
 	found, reasons, score := PodFitsGroupConstraints(nodeInfo, spec)
@@ -132,6 +163,50 @@ func sampleTest(pod *v1.Pod, nodeInfo *schedulercache.NodeInfo) {
 	}
 }
 
+func testContainerAllocs(t *testing.T, conts []cont, podConts []v1.Container) {
+	if len(conts) != len(podConts) {
+		t.Errorf("Number of containers don't match - expected %v - have %v", len(conts), len(podConts))
+		return
+	}
+	for ci, c := range conts {
+		if len(c.expectedGrpLoc) != len(podConts[ci].Resources.AllocateFrom) {
+			t.Errorf("Number of resources don't match - expected %v %v - have %v %v",
+				len(c.expectedGrpLoc), c.expectedGrpLoc,
+				len(podConts[ci].Resources.AllocateFrom), podConts[ci].Resources.AllocateFrom)
+			return
+		}
+		for key, val := range c.expectedGrpLoc {
+			valP, available := podConts[ci].Resources.AllocateFrom[v1.ResourceName(key)]
+			if !available {
+				t.Errorf("Expected key %v not available", key)
+			} else if string(valP) != val {
+				t.Errorf("Expected value for key %v not same - expected %v - have %v",
+					key, val, valP)
+			}
+		}
+	}
+}
+
+func testPodAllocs(t *testing.T, pod *v1.Pod, podEx *PodEx, nodeInfo *schedulercache.NodeInfo) {
+	spec := &pod.Spec
+	found, _, score := PodFitsGroupConstraints(nodeInfo, spec)
+	if found {
+		if podEx.rcont[0].expectedGrpLoc == nil {
+			t.Errorf("Group allocation found when it should not be found")
+		} else {
+			if math.Abs(score-podEx.expectedScore)/podEx.expectedScore > 0.01 {
+				t.Errorf("Score not correct - expected %v - have %v", podEx.expectedScore, score)
+			}
+			testContainerAllocs(t, podEx.icont, pod.Spec.InitContainers)
+			testContainerAllocs(t, podEx.rcont, pod.Spec.Containers)
+		}
+	} else {
+		if podEx.rcont[0].expectedGrpLoc != nil {
+			t.Errorf("Group allocation not found when it should be found")
+		}
+	}
+}
+
 func TestGrpAllocate1(t *testing.T) {
 	flag.Parse()
 
@@ -143,26 +218,37 @@ func TestGrpAllocate1(t *testing.T) {
 			"gpu/dev1/memory": 256000, "gpu/dev1/cards": 1, "gpu/dev1/enumType": int64(0x1),
 			"gpu/dev2/memory": 257000, "gpu/dev2/cards": 1,
 			"gpu/dev3/memory": 192000, "gpu/dev3/cards": 1, "gpu/dev3/enumType": int64(0x1),
-			"gpu/dev4/memory": 178000, "gpu/dev4/cards": 1})
+			"gpu/dev4/memory": 178000, "gpu/dev4/cards": 1},
+	)
 
 	// required resources
-	pod := createPod("pod1",
+	pod, podEx := createPod("pod1", 2.994582,
 		[]cont{
 			{name: "Init0",
-				res:    map[string]int64{"A1": 2200, "B1": 2000},
-				grpres: map[string]int64{"gpu/0/memory": 100000, "gpu/0/cards": 1}}},
+				res:            map[string]int64{"A1": 2200, "B1": 2000},
+				grpres:         map[string]int64{"gpu/0/memory": 100000, "gpu/0/cards": 1},
+				expectedGrpLoc: map[string]string{"gpu/0": "gpu/dev4"}}},
 		[]cont{
 			{"Run0",
 				map[string]int64{"A1": 3000, "B1": 1000},
 				map[string]int64{
 					"gpu/a/memory": 256000, "gpu/a/cards": 1,
-					"gpu/b/memory": 178000, "gpu/b/cards": 1}},
+					"gpu/b/memory": 178000, "gpu/b/cards": 1},
+				map[string]string{
+					"gpu/a": "gpu/dev2",
+					"gpu/b": "gpu/dev4"},
+			},
 			{name: "Run1",
 				res: map[string]int64{"A1": 1000, "B1": 2000},
 				grpres: map[string]int64{
-					"gpu/0/memory": 190000, "gpu/0/cards": 1, "gpu/0/enumType": int64(0x3)}}})
+					"gpu/0/memory": 190000, "gpu/0/cards": 1, "gpu/0/enumType": int64(0x3)},
+				expectedGrpLoc: map[string]string{"gpu/0": "gpu/dev3"},
+			},
+		},
+	)
 
-	sampleTest(pod, nodeInfo)
+	//sampleTest(pod, podEx, nodeInfo)
+	testPodAllocs(t, pod, podEx, nodeInfo)
 
 	glog.Flush()
 }
